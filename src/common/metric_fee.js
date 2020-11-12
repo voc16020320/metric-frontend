cc387685d1c7e57ad6510749209a40bbc30802a7
talegift <noname@no-domain.com>
2020-11-13 00:03:43 +0700

    increase min METRIC to get no fee to 1000

--- src/common/metric_fee.js
+++ src/common/metric_fee.js
@@ -3,7 +3,7 @@ import {tokensList} from "./token_fetch";
 export let MetricReferralAddress = "0x52427b0035F494a21a0A4A1AbE04d679f789c821"
 
 export function calculateMetricFee() {
-    return fetchMetricBalance() >= 100 ? 0 : MetricFee
+    return fetchMetricBalance() >= 1000 ? 0 : MetricFee
 }
 
 export function fetchMetricBalance() {
