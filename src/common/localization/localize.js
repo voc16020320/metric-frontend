3609ca8b94c85f7b0e667a4cdf25b2cd2b7ad85c
SHA-2048 <8516712+SHA-2048@users.noreply.github.com>
2020-11-19 08:39:18 +0100

    [refactor] Integrate new CN/TW language translations + simplify language loading

--- src/common/localization/localize.js
+++ src/common/localization/localize.js
@@ -1,8 +1,17 @@
 import i18next from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from './translations/en.json'
-import fr from './translations/fr.json'
+import en from './translations/en-US.json'
+import fr from './translations/fr-FR.json'
+import cn from './translations/zh-CN.json'
+import tw from './translations/zh-TW.json'
+
+export let supportedLanguages = {
+    'en-US': en,
+    'fr-FR': fr,
+    'zh-CN': cn,
+    'zh-TW': tw
+}
 
 export async function initLocalizations() {
     return i18next
@@ -10,9 +19,6 @@ export async function initLocalizations() {
         .init({
             fallbackLng: 'en',
             debug: false,
-            resources: {
-                en: en,
-                fr: fr
-            }
+            resources: supportedLanguages
         }, function(err, t) {})
 }
